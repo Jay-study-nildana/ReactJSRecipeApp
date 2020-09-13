224d3f7cbda8fff220d9f32321b8914a9e01069b
Jay <vijayasimhabr@live.com>
2020-09-13 23:41:39 +0530

    trying again 1141

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,7 +5,7 @@ function Menu({ recipes }) {
   return (
     <article>
       <header>
-        <h1>Delicious Recipes</h1>
+        <h1>Delicious Recipes BROOOOOOOOOOOO</h1>
       </header>
       <div className="recipes">
         {recipes.map((recipe, i) => (
