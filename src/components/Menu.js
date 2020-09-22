e83626637f22da63d67baa26534bb0d0bc0afa60
Vijayasimha BR <vijayasimhabr@live.com>
2020-09-23 01:53:19 +0530

    testing again 153

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,7 +12,7 @@ function Menu({ recipes }) {
         {/* <h1 className="alert">Delicious Recipes because I am Batman</h1> */}
         <h1>
           <Alert color="primary">
-          Delicious Recipes. Because. I am Batman. Also, Robin.
+          Delicious Recipes. Because. I am Batman. Also, Robin - 153. 
           </Alert>        
         </h1>
 
